f3e79cce5b2a7a486df04267463846fed12018cc
datnv <datnvhe130026@fpt.edu.vn>
2023-05-24 10:28:27 +0700

    use Ajax and call API

--- Admin/Admin.js
+++ Admin/Admin.js
@@ -2,77 +2,39 @@ $(function () {
   //
   loadComponent();
   //
-  loadProductToTable();
+  loadProduct();
   //
+  generateManufacturer();
+  //
+  generateCategory();
 });
 
 // function to load components
-function loadComponent(params) {
+function loadComponent() {
   $(".MenuSection").load("./Menu.html");
   $(".SidebarSection").load("./Sidebar.html");
   // $(".ContentProduct").load("./ContentProduct.html");
 }
 
 // function to process when click on "menu item Product"
-function handleShowProduct(params) {
+function handleShowProduct() {
   $(".ContentProduct").load("./ContentProduct.html");
 }
 
 // function to process when click on "menu item manufacturer"
-function handleShowManufacturer(params) {
+function handleShowManufacturer() {
   $(".ContentProduct").load("./ContentManufacturer.html");
 }
 
 // function to process when click on "menu item account"
-function handleShowAccount(params) {
+function handleShowAccount() {
   $(".ContentProduct").load("./ContentAccount.html");
 }
 
 // function to process when click on "menu item category"
-function handleShowCategory(params) {
+function handleShowCategory() {
   $(".ContentProduct").load("./ContentCategory.html");
 }
-
-// function to set timout for modal
-// $("#modal-id").modal("show").on("shown", function () {
-//     window.setTimeout(function () {
-//         $("#modal-id").modal("hide");
-//     }, 5000);
-// });
-
-// function to load data to table
-function loadProductToTable() {
-  // reset table before load data
-  $("#tbProductTable").empty();
-  // create product by loop
-  for (let index = 0; index < 6; index++) {
-    $("#tbProductTable").append(`
-        <tr>
-                                <td>1</td>
-                                <td>Samsung Galaxy S22 Ultra 5G</td>
-                                <td>30.090.000đ</td>
-                                <td>6.9 inches, Chip Media Tek Helio G85 (12nm) mạnh mẽ, Ram 4G, Pin
-                                    7000 mAh</td>
-                                <td>ProductDetail1</td>
-                                <td>2</td>
-                                <td>ImgMobile5.png</td>
-                                <td>APPLE</td>
-                                <td>TABLET</td>
-                                <td>
-                                    <button type="button" class="btn btn-warning">Edit</button>
-                                </td>
-                                <td>
-                                    <button type="button" class="btn btn-danger">Delete</button>
-                                </td>
-                            </tr> 
-
-        `);
-  }
-  // load data from local Storage to table
-  loadProduct();
-}
-// declare listProduct[]
-var listProduct = [];
 
 // function handle create new product
 function handleCreateNewProduct() {
@@ -86,13 +48,14 @@ function handleCreateNewProduct() {
   var p_Image = getImageName($("#Image").val());
   var p_Manufacturer = $("#Manufacturer").val();
   var p_Category = $("#Category").val();
+  //
 
   // create a object
   var productNew = {
-    id: p_Id,
+    // id: p_Id,
     name: p_Name,
     price: p_Price,
-    infor: p_Info,
+    info: p_Info,
     detail: p_Detail,
     ratingStar: p_Star,
     imageName: p_Image,
@@ -100,19 +63,31 @@ function handleCreateNewProduct() {
     categoryId: p_Category,
   };
 
-  // save to list to use
-  listProduct.push(productNew);
-  console.log(listProduct);
+  console.log(productNew);
 
-  // Save to localStorage
-  localStorage.setItem("listProduct", JSON.stringify(listProduct));
+  // username and password to login
+  var userName = "admin";
+  var passWord = "123456";
+  // Call API
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8080/api/v1/products",
+    headers: {
+      Authorization: "Basic " + btoa(userName + ":" + passWord),
+    },
+    data: JSON.stringify(productNew),
+    contentType: "application/json;charset=UTF-8",
+    // dataType: "dataType",
+    success: function (response) {
+      // close form add new
+      $("#close").click();
+      $(".modal-backdrop").remove();
 
-  // reload list product in table
-  loadProduct();
-
-  // close form add new
-  $("#close").click();
-  $(".modal-backdrop").remove();
+      loadProduct();
+      location.reload();
+      alert("Add new successfully !");
+    },
+  });
 }
 
 // function to reset form add new product
@@ -137,33 +112,36 @@ function getImageName(pathImage) {
   return imageName;
 }
 
-// function load data from loacalStorage to table
+// declare global variable
+let listProduct = [];
+// function load data  to table
 function loadProduct() {
-  //  listProduct
-  let listProduct = [];
-  // get data from localStorage to use
-  // check data in localStorage
-  if (localStorage && localStorage.getItem("listProduct")) {
-    var listProductLocalStorage = JSON.parse(
-      localStorage.getItem("listProduct")
-    );
-    // save data from loacalStorage to use
-    listProduct = listProductLocalStorage;
-  }
-
-  // addnew
-  for (let index = 0; index < listProduct.length; index++) {
-    $("#tbProductTable").append(`
+  // username and pass
+  var username = "admin";
+  var passWord = "123456";
+  // call API to get products
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/api/v1/products",
+    headers: {
+      Authorization: "Basic " + btoa(username + ":" + passWord),
+    },
+    success: function (response) {
+      // response is result
+      listProduct = response.content;
+      // load to table
+      for (let index = 0; index < listProduct.length; index++) {
+        $("#tbProductTable").append(`
     <tr>
                             <td>${listProduct[index].id}</td>
                             <td>${listProduct[index].name}</td>
                             <td>${listProduct[index].price}</td>
-                            <td>${listProduct[index].infor}</td>
+                            <td>${listProduct[index].info}</td>
                             <td>${listProduct[index].detail}</td>
                             <td>${listProduct[index].ratingStar}</td>
                             <td>${listProduct[index].imageName}</td>
-                            <td>${listProduct[index].manufacturerId}</td>
-                            <td>${listProduct[index].categoryId}</td>
+                            <td>${listProduct[index].manufacturerName}</td>
+                            <td>${listProduct[index].categoryName}</td>
                             <td>
                                 <button type="button" class="btn btn-warning"
                                  data-toggle="modal" data-target="#myUpdateModal"
@@ -175,53 +153,65 @@ function loadProduct() {
                         </tr> 
 
     `);
-  }
+      }
+    },
+  });
 }
 
 // function delete product
 function handleDeleteProduct(idProductDelete) {
   // alert a box to confirm yes or no delete
   var confirmDelete = confirm("Bạn muốn xóa sản phẩm này ?");
-
+  // userName and password to login authentication
+  var userName = "admin";
+  var passWord = "123456";
+  // condition
   if (confirmDelete) {
-    // delete
-    listProduct.splice(idProductDelete);
-    // re-save listProduct on localStorage
-    localStorage.setItem("listProduct", JSON.stringify(listProduct));
-    // re-load product on table
-    loadProducttoTable();
+    $.ajax({
+      type: "DELETE",
+      url: `http://localhost:8080/api/v1/products/${idProductDelete}`,
+      headers: {
+        Authorization: "Basic " + btoa(userName + ":" + passWord),
+      },
+      success: function (response) {
+        //re-load product on table
+        loadProduct();
+        location.reload();
+        alert("Delete successfully !");
+      },
+    });
   }
 }
 
 // function update product
 function handleEditProduct(idProductUpdate) {
   // fill data into textfield of update modal
-  fillData(idProductUpdate);
+  fillDataToModalUpdate(idProductUpdate);
 }
 
 // function to fill data into textfield of update modal
-function fillData(idProduct) {
-  listProduct = [];
-  // get data from localStorage
-  var listProductLocal = JSON.parse(localStorage.getItem("listProduct"));
-  // save to listProduct[] to use
-  listProduct = listProductLocal;
-  console.log(listProduct);
-  // loop to find data of product want to update
-  for (let index = 0; index < listProduct.length; index++) {
-    if (listProduct[index].id == idProduct) {
-      // set value for textfield of update modal
-      $("#Id_Update").val(idProduct);
-      $("#Name_Update").val(listProduct[index].name);
-      $("#Price_Update").val(listProduct[index].price);
-      $("#Info_Update").val(listProduct[index].infor);
-      $("#Detail_Update").val(listProduct[index].detail);
-      $("#Star_Update").val(listProduct[index].ratingStar);
-      // $("#Image_Update").val(listProduct[index].imageName);
-      $("#Manufacturer_Update").val(listProduct[index].manufacturerId);
-      $("#Category_Update").val(listProduct[index].categoryId);
-    }
-  }
+function fillDataToModalUpdate(idProduct) {
+  // declare variable
+  var index = listProduct.findIndex((product) => product.id == idProduct);
+  // manufacturerId
+  var manufacturerId = listManufacture.find(
+    (manufact) => manufact.name == listProduct[index].manufacturerName
+  ).id;
+  // category id
+  var categoryid = listCategory.find(
+    (category) => category.name == listProduct[index].categoryName
+  ).id;
+
+  // set value for textfield of update modal
+  $("#Id_Update").val(idProduct);
+  $("#Name_Update").val(listProduct[index].name);
+  $("#Price_Update").val(listProduct[index].price);
+  $("#Info_Update").val(listProduct[index].info);
+  $("#Detail_Update").val(listProduct[index].detail);
+  $("#Star_Update").val(listProduct[index].ratingStar);
+  // $("#Image_Update").val(getImageName(listProduct[index].imageName));
+  $("#Manufacturer_Update").val(manufacturerId);
+  $("#Category_Update").val(categoryid);
 }
 
 // function reset data on textfield of update form
@@ -249,26 +239,176 @@ function handleUpdateProduct() {
   var u_Manufacturer = $("#Manufacturer_Update").val();
   var u_Category = $("#Category_Update").val();
 
-  for (let index = 0; index < listProduct.length; index++) {
-    if (listProduct[index].id == u_Id) {
-      listProduct[index].name = u_Name;
-      listProduct[index].price = u_Price;
-      listProduct[index].infor = u_Info;
-      listProduct[index].detail = u_Detail;
-      listProduct[index].ratingStar = u_Star;
-      listProduct[index].imageName = u_Image;
-      listProduct[index].manufacturerId = u_Manufacturer;
-      listProduct[index].categoryId = u_Category;
-
-      // save to localStorage
-      localStorage.setItem("listProduct", JSON.stringify(listProduct));
-      
+  // Update
+  let productUpdate = {
+    name: u_Name,
+    price: u_Price,
+    info: u_Info,
+    detail: u_Detail,
+    ratingStar: u_Star,
+    imageName: u_Image,
+    manufacturerId: u_Manufacturer,
+    categoryId: u_Category,
+  };
+  // username and password to login authentication
+  var userName = "admin";
+  var passWord = "123456";
+  // call API
+  $.ajax({
+    type: "PUT",
+    url: `http://localhost:8080/api/v1/products/${u_Id}`,
+    headers: {
+      Authorization: "Basic " + btoa(userName + ":" + passWord),
+    },
+    data: JSON.stringify(productUpdate),
+    contentType: "application/json;charset=UTF-8",
+    success: function (response) {
       // close modal
       $("#myUpdateModal").modal("hide");
       $(".modal-backdrop").remove();
       // re-load table
-      loadProductToTable();
+      loadProduct();
+      alert("Update successfully !");
+      location.reload();
+    },
+  });
 
+  // find index of product in list product
+  // var indexUpdate = listProduct.findIndex((product) => product.id == u_Id);
+  // console.log("indexUpdate : ", indexUpdate);
+
+  // listProduct[index].name = u_Name;
+  // listProduct[index].price = u_Price;
+  // listProduct[index].infor = u_Info;
+  // listProduct[index].detail = u_Detail;
+  // listProduct[index].ratingStar = u_Star;
+  // listProduct[index].imageName = u_Image;
+  // listProduct[index].manufacturerId = u_Manufacturer;
+  // listProduct[index].categoryId = u_Category;
+
+  // save to localStorage
+  // localStorage.setItem("listProduct", JSON.stringify(listProduct));
+
+  // close modal
+  // $("#myUpdateModal").modal("hide");
+  // $(".modal-backdrop").remove();
+  // re-load table
+  // loadProduct();
+  // alert("Update successfully !");
+  // location.reload();
+  //   }
+  // }
+}
+
+// function to search by name product
+function handleToSearch() {
+  // get search value
+  var searchValue = $("#inputSearch").val();
+  //  listProduct
+  let listProduct = [];
+  // get data  to use
+  // check data in localStorage
+  if (localStorage && localStorage.getItem("listProduct")) {
+    var listProductLocalStorage = JSON.parse(
+      localStorage.getItem("listProduct")
+    );
+    // save data from loacalStorage to use
+    listProduct = listProductLocalStorage;
+  }
+  // clear table
+  //  $("#tbProductTable").clear();
+  // $("#tbProductTable").remove();
+  $("#tbProductTable").empty();
+  // load to table
+  for (let index = 0; index < listProduct.length; index++) {
+    if (listProduct[index].name.includes(searchValue)) {
+      $("#tbProductTable").append(`
+    <tr>
+                            <td>${listProduct[index].id}</td>
+                            <td>${listProduct[index].name}</td>
+                            <td>${listProduct[index].price}</td>
+                            <td>${listProduct[index].infor}</td>
+                            <td>${listProduct[index].detail}</td>
+                            <td>${listProduct[index].ratingStar}</td>
+                            <td>${listProduct[index].imageName}</td>
+                            <td>${listProduct[index].manufacturerId}</td>
+                            <td>${listProduct[index].categoryId}</td>
+                            <td>
+                                <button type="button" class="btn btn-warning"
+                                 data-toggle="modal" data-target="#myUpdateModal"
+                                 onClick="handleEditProduct(${listProduct[index].id})">Edit</button>
+                            </td>
+                            <td>
+                                <button type="button" class="btn btn-danger" onClick="handleDeleteProduct(${listProduct[index].id})">Delete</button>
+                            </td>
+                        </tr> 
+
+    `);
+      // case : input is empty
+    } else if (searchValue == "") {
+      loadProduct();
     }
   }
+}
+
+// declare variable global
+var listManufacture = [];
+// fuction call API to generate Manufacturer
+function generateManufacturer() {
+  // username and password
+  var userName = "admin";
+  var passWord = "123456";
+  // Call API
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/api/v1/manufacturers",
+    headers: {
+      Authorization: "Basic " + btoa(userName + ":" + passWord),
+    },
+    success: function (response) {
+      listManufacture = response;
+      // loop
+      for (let index = 0; index < listManufacture.length; index++) {
+        // add new modal
+        $("#Manufacturer").append(`
+        <option value="${listManufacture[index].id}">${listManufacture[index].name}</option>
+        `);
+        // update modal
+        $("#Manufacturer_Update").append(`
+        <option value="${listManufacture[index].id}">${listManufacture[index].name}</option>
+        `);
+      }
+    },
+  });
+}
+
+// declare variable global
+var listCategory = [];
+// fuction call API to generate Category
+function generateCategory() {
+  // username and password
+  var userName = "admin";
+  var passWord = "123456";
+  // Call API
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/api/v1/categorys",
+    headers: {
+      Authorization: "Basic " + btoa(userName + ":" + passWord),
+    },
+    success: function (response) {
+      listCategory = response;
+      // loop
+      for (let index = 0; index < listCategory.length; index++) {
+        // category - add new modal
+        $("#Category").append(`
+        <option value="${listCategory[index].id}">${listCategory[index].name}</option>
+        `);
+        // category - update modal
+        $("#Category_Update").append(`
+        <option value="${listCategory[index].id}">${listCategory[index].name}</option>
+        `);
+      }
+    },
+  });
 }
--- Home/Home.js
+++ Home/Home.js
@@ -2,7 +2,7 @@ $(function () {
   //
   loadComponent();
   setTimeout(() => {
-    generateProduct();
+    loadProduct();
   }, 200);
 });
 // function to load components
@@ -13,144 +13,89 @@ function loadComponent(params) {
   $(".FooterSection").load("./Footer.html");
 }
 
-//  Function generate products
-function generateProduct(params) {
-  // Template string
-  for (let index = 0; index < 10; index++) {
-    $(".ProductList").append(`
-    <!-- product 1 -->
-    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-        <!-- image -->
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <img src="../Asset/Product/ImgMobile1.PNG" alt="" style="width: 160px;
-                height: 190px">
-            </div>
-        </div>
-        <!-- title -->
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <h4 style="font-weight: bold;">Samsung Galaxy S22 Ultra 5G</h4>
-            </div>
-        </div>
-        <!-- manufacturer -->
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <p>Hãng sản xuất : Apple</p>
-            </div>
-        </div>
-        <!-- star image -->
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <ul style="list-style: none;
-                display: flex;
-                padding: 0 2px;">
-                    <li>
-                        <i class="fa fa-star" style="color: orange;"></i>
-                    </li>
-                    <li>
-                        <i class="fa fa-star" style="color: orange;"></i>
-                    </li>
-                    <li>
-                        <i class="fa fa-star" style="color: orange;"></i>
-                    </li>
-                    <li>
-                        <i class="fa fa-star" style="color: orange;"></i>
-                    </li>
-                    <li>
-                        <i class="fa fa-star"></i>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-        <!-- price and cart -->
-
-        <div class="row">
-            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                <p>25.000.000vnđ</p>
-            </div>
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <a href="#">
-                    <i class="fa fa-shopping-cart" style="color: red;font-size: 25px;"></i>
-                </a>
-            </div>
-        </div>
-    </div>
-    </div>
-    `);
-  }
-  loadProduct();
-}
-
 // declare listProduct
 var listProduct = [];
-// function to get data from localStorage, then generate product to table
+// function call API to get data from localStorage, then generate product to table
 function loadProduct() {
-  // get data from localStorage to use
-  // check yes or no data on LocalStorage ?
-  if (localStorage && localStorage.getItem("listProduct")) {
-    var listProductLocalStorage = JSON.parse(
-      localStorage.getItem("listProduct")
-    );
-    // save data from localStorage to listProduct to use
-    listProduct = listProductLocalStorage;
-  }
+  // username and pass to login auth
+  var uName = "admin";
+  var passWrd = "123456";
+  // use Ajax to Call API to get listProduct
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/api/v1/products",
+    // method 1 :
+    headers: {
+      Authorization: "Basic " + btoa(uName + ":" + passWrd),
+    },
+    success: function (response, status) {
+      //
+      listProduct = response.content;
+      //
+      for (let index = 0; index < listProduct.length; index++) {
+        $(".ProductList").append(`
+              <!-- product from localStorage -->
+              <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                  <!-- image -->
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                          <img src="../Asset/Product/${
+                            listProduct[index].imageName
+                          }" alt="" style="width: 160px;
+                          height: 190px">
+                      </div>
+                  </div>
+                  <!-- title -->
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                          <h4 style="font-weight: bold;">${
+                            listProduct[index].name
+                          }</h4>
+                      </div>
+                  </div>
+                  <!-- manufacturer -->
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                          <p>Hãng sản xuất : ${
+                            listProduct[index].manufacturerName
+                          }</p>
+                      </div>
+                  </div>
+                  <!-- star image -->
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                          <ul style="list-style: none;
+                          display: flex;
+                          padding: 0 2px;">
+                              ${showStar(listProduct[index].ratingStar)}
+                          </ul>
+                      </div>
+                  </div>
+          
+                  <!-- price and cart -->
+          
+                  <div class="row">
+                      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                          <p>${listProduct[index].price}</p>
+                      </div>
+                      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                          <a href="#">
+                              <i class="fa fa-shopping-cart" style="color: red;font-size: 25px;"></i>
+                          </a>
+                      </div>
+                  </div>
+              </div>
+              </div>
+              `);
+      }
+    },
 
-  for (let index = 0; index < listProduct.length; index++) {
-    $(".ProductList").append(`
-        <!-- product from localStorage -->
-        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-            <!-- image -->
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <img src="../Asset/Product/${
-                      listProduct[index].imageName
-                    }" alt="" style="width: 160px;
-                    height: 190px">
-                </div>
-            </div>
-            <!-- title -->
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <h4 style="font-weight: bold;">${
-                      listProduct[index].name
-                    }</h4>
-                </div>
-            </div>
-            <!-- manufacturer -->
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <p>Hãng sản xuất : ${listProduct[index].manufacturerId}</p>
-                </div>
-            </div>
-            <!-- star image -->
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <ul style="list-style: none;
-                    display: flex;
-                    padding: 0 2px;">
-                        ${showStar(listProduct[index].ratingStar)}
-                    </ul>
-                </div>
-            </div>
-    
-            <!-- price and cart -->
-    
-            <div class="row">
-                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                    <p>${listProduct[index].price}</p>
-                </div>
-                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                    <a href="#">
-                        <i class="fa fa-shopping-cart" style="color: red;font-size: 25px;"></i>
-                    </a>
-                </div>
-            </div>
-        </div>
-        </div>
-        `);
-  }
+    error: function (xhr, ajaxOptions, throwError) {
+      //Error block
+      console.log("Error !");
+    },
+  });
+  //
 }
 
 // function to display number of star
@@ -160,18 +105,18 @@ function showStar(numberOfStars) {
   // display active stars
   for (let index = 0; index < numberOfStars; index++) {
     totalStars += `
-        <li>
-            <i class="fa fa-star" style="color: orange;"></i>
-        </li>
-        `;
+          <li>
+              <i class="fa fa-star" style="color: orange;"></i>
+          </li>
+          `;
   }
   // display unactive stars
   for (let index = 0; index < 5 - numberOfStars; index++) {
     totalStars += `
-        <li>
-            <i class="fa fa-star"></i>
-        </li>
-        `;
+          <li>
+              <i class="fa fa-star"></i>
+          </li>
+          `;
   }
 
   return totalStars;
